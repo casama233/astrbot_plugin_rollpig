65e02a4a65a4e9a98140471ccd2cde385a48a04c
sama233 <97009482+casama233@users.noreply.github.com>
2026-08-03 23:52:23 +0800

    feat: export SQLite storage manager

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,4 +1,12 @@
 from .base import StorageBackend
 from .json_storage import JSONStorage
+from .manager import StorageManager, StorageMigrationError
+from .sqlite_storage import SQLiteStorage
 
-__all__ = ["StorageBackend", "JSONStorage"]
+__all__ = [
+    "StorageBackend",
+    "JSONStorage",
+    "SQLiteStorage",
+    "StorageManager",
+    "StorageMigrationError",
+]
