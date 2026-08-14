b6f621c8bc5be9501e7737b74215b6e2c173112f
sama233 <97009482+casama233@users.noreply.github.com>
2026-08-15 00:21:25 +0800

    refactor: export collection service

--- services/__init__.py
+++ services/__init__.py
@@ -1,10 +1,12 @@
 from .catalog_service import CatalogService
+from .collection_service import CollectionService
 from .draw_service import DrawService
 from .resource_read_service import ResourceReadService
 from .roast_service import RoastService
 
 __all__ = [
     "CatalogService",
+    "CollectionService",
     "DrawService",
     "ResourceReadService",
     "RoastService",
