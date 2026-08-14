79c5ecd2bae8d7fef02a1146397fcab221bbd35c
sama233 <97009482+casama233@users.noreply.github.com>
2026-08-15 01:19:45 +0800

    feat: export oven refill service

--- services/__init__.py
+++ services/__init__.py
@@ -1,6 +1,7 @@
 from .catalog_service import CatalogService
 from .collection_service import CollectionService
 from .draw_service import DrawService
+from .oven_refill_service import OvenRefillService
 from .resource_read_service import ResourceReadService
 from .roast_service import RoastService
 
@@ -8,6 +9,7 @@ __all__ = [
     "CatalogService",
     "CollectionService",
     "DrawService",
+    "OvenRefillService",
     "ResourceReadService",
     "RoastService",
 ]
