6f4fad1329d017c63cfeaf96f84935f23d256863
sama233 <97009482+casama233@users.noreply.github.com>
2026-08-23 00:36:48 +0800

    chore: export eat service

--- services/__init__.py
+++ services/__init__.py
@@ -1,6 +1,7 @@
 from .catalog_service import CatalogService
 from .collection_service import CollectionService
 from .draw_service import DrawService
+from .eat_service import EatService
 from .oven_refill_service import OvenRefillService
 from .resource_read_service import ResourceReadService
 from .roast_service import RoastService
@@ -9,6 +10,7 @@ __all__ = [
     "CatalogService",
     "CollectionService",
     "DrawService",
+    "EatService",
     "OvenRefillService",
     "ResourceReadService",
     "RoastService",
